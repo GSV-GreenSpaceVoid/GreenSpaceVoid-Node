613932c86d02ff62dcd7747455f92f550f23fe4c
GSV-GreenSpaceVoid <79030193+GSV-GreenSpaceVoid@users.noreply.github.com>
2021-10-06 19:18:36 -0500

    Added the first fighter

---
+++ core/src/main/java/com/greenspacevoidnode/common/entities/faction/israel/ship/military/fighter/Gideon.java
@@ -0,0 +1,130 @@
+package com.greenspacevoidnode.common.entities.faction.israel.ship.military.fighter;
+
+import com.greenspacevoidnode.common.core.entity.vessel.ship.Ship;
+
+public class Gideon extends Ship {
+
+    private static double baseStructureHP, baseHullHP, baseShieldHP;
+    private static double baseShieldRegenerationRate;
+
+    private static double baseStructureEnergyResist;
+    private static double baseStructureThermalResist;
+    private static double baseStructureEMPResist;
+    private static double baseStructureExplosiveResist;
+    private static double baseHullEnergyResist;
+    private static double baseHullThermalResist;
+    private static double baseHullEMPResist;
+    private static double baseHullExplosiveResist;
+    private static double baseShieldEnergyResist;
+    private static double baseShieldThermalResist;
+    private static double baseShieldEMPResist;
+    private static double baseShieldExplosiveResist;
+
+
+    private static double baseSensorStrength;
+    private static double BaseSensorRadius;
+    private static double baseSignatureStrength;
+    private static double baseSignatureRadius;
+
+
+
+    
+
+
+
+
+
+    private static double basePowerGrid;
+    private static double baseCapacitorCapacity;
+    private static double baseCapacitorRegenerationRate;
+
+    private static double baseInertiaModifier;
+    private static double baseSpeed;
+    private static double baseAccelerationRate;
+    private static double baseTurnRate;
+
+    private static double baseCargoHoldCapacity;
+
+    private static int hullModuleSlots;
+    private static int armorModuleSlots;
+    private static int shieldModuleSlots;
+    private static int weaponModuleSlots;
+    private static int miningModuleSlots;
+    private static int weaponSlots;
+    private static int maxTargets;
+
+    private static double baseRailGunDamageModifier;
+    private static double baseMissileDamageModifier;
+    private static double baseLaserDamageModifier;
+    private static double baseArtilleryDamageModifier;
+    private static double baseMiningYieldModifier;
+    private static double baseRailGunROFModifier;
+    private static double baseMissileROFModifier;
+    private static double baseLaserROFModifier;
+    private static double baseArtilleryROFModifier;
+    private static double baseMiningROFModifier;
+
+    private static double baseDroneControlRange;
+    private static double maxNumberOfDrones;
+    private static double baseDroneDamageModifier;
+    private static double baseDroneDefenseModifier;
+    private static double baseDroneSpeedModifier;
+    private static double baseDroneRateOfFireModifier;
+    private static double baseDroneMiningYieldModifier;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public Gideon(String name, long systemID, long x, long y, double volume, boolean isInvincible, boolean isTargetable, boolean canMove, double baseCargoCapacity) {
+        super(name, systemID, x, y, volume, isInvincible, isTargetable, canMove, baseCargoCapacity);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
